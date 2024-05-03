f4427dbd423fe441287da6a444cfada24e822b63
davidbarone <davidbarone@live.com>
2024-05-03 22:23:18 +1000

    Example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Commentario
 A VS Code extension to format C# xml comments.
+
+## Example
+For an example of the output documentation, refer to: [ExampleLibrary.html](https://github.com/davidbarone/Commentario/blob/main/ExampleLibrary.html.md).
--- Server/Dbarone.Net.CommentarioServer.Tests/CommentarioServerTests/DocumentGeneratorTests.cs
+++ Server/Dbarone.Net.CommentarioServer.Tests/CommentarioServerTests/DocumentGeneratorTests.cs
@@ -12,7 +12,7 @@ public class DocumentGeneratorTests {
     public void BasicTests() {
         string xmlCommentsPath = @"..\..\..\..\ExampleLibrary\bin\Debug\net8.0\ExampleLibrary.xml";
         string assemblyPath = @"..\..\..\..\ExampleLibrary\bin\Debug\net8.0\ExampleLibrary.dll";
-        string outputPath = @"..\..\..\..\ExampleLibrary\bin\Debug\net8.0\ExampleLibrary.html";
+        string outputPath = @"..\..\..\..\..\ExampleLibrary.html";   // place in Commentario root folder, so gets checked into GitHub
 
         DocumentGenerator dg = DocumentGenerator.Create(xmlCommentsPath, assemblyPath, outputPath, OutputType.Html);
         dg.GenerateDocument();
