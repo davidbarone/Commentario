0ba8c13d903bbb71fa0ed5b5ecdd511572f789d9
davidbarone <davidbarone@live.com>
2024-05-12 14:46:12 +1000

    Console + launch task

--- Server/Dbarone.Net.CommentarioServer.Tests/CommentarioServerTests/DocumentGeneratorTests.cs
+++ Server/Dbarone.Net.CommentarioServer.Tests/CommentarioServerTests/DocumentGeneratorTests.cs
@@ -18,18 +18,28 @@ public class DocumentGeneratorTests
         string readmePath = @"..\..\..\..\ExampleLibrary\readme.txt";
         string outputPath = @"..\..\..\..\..\ExampleLibrary.html";   // place in Commentario root folder, so gets checked into GitHub
 
-        DocumentGenerator dg = DocumentGenerator.Create(xmlCommentsPath, assemblyPath, readmePath, outputPath, OutputType.Html);
+        DocumentGenerator dg = DocumentGenerator.Create(
+            assemblyPath,
+            outputPath,
+            OutputType.Html,
+            xmlCommentsPath, readmePath);
         dg.GenerateDocument();
     }
 
     [Fact]
-    public void CreateCommentarioDocumentation() {
+    public void CreateCommentarioDocumentation()
+    {
         string xmlCommentsPath = @"..\..\..\..\Dbarone.Net.CommentarioServer\bin\Debug\net8.0\Dbarone.Net.CommentarioServer.xml";
         string assemblyPath = @"..\..\..\..\Dbarone.Net.CommentarioServer\bin\Debug\net8.0\Dbarone.Net.CommentarioServer.dll";
         string readmePath = @"..\..\..\..\Dbarone.Net.CommentarioServer\readme.txt";
         string outputPath = @"..\..\..\..\..\Dbarone.Net.CommentarioServer.html";   // place in Commentario root folder, so gets checked into GitHub
 
-        DocumentGenerator dg = DocumentGenerator.Create(xmlCommentsPath, assemblyPath, readmePath, outputPath, OutputType.Html);
+        DocumentGenerator dg = DocumentGenerator.Create(
+            assemblyPath,
+            outputPath,
+            OutputType.Html,
+            xmlCommentsPath,
+            readmePath);
         dg.GenerateDocument();
 
     }
