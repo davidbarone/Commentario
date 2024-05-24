67bb860ca27a7d11ca79ceaf46a30fc9a0bd7ed2
davidbarone <davidbarone@live.com>
2024-05-25 09:59:29 +1000

    testing

--- Server/Dbarone.Net.CommentarioServer.Tests/CommentarioServerTests/XmlCommentReaderTests.cs
+++ Server/Dbarone.Net.CommentarioServer.Tests/CommentarioServerTests/XmlCommentReaderTests.cs
@@ -43,6 +43,39 @@ public class XmlCommentReaderTests
         }
     }
 
+    [Fact]
+    public void LoadCommentsWithSingleMemberCommentFull()
+    {
+        var xmlText = @"<?xml version=""1.0""?>
+<doc>
+    <assembly>
+        <name>f
+            ExampleLibrary
+        </name>
+    </assembly>
+    <members>
+        <member name=""M:ExampleLibrary.Test.GenericMethod()"">
+            <summary>A method.</summary>
+            <typeparam name=""T"">The type parameter 'T'.</typeparam>
+            <param name=""a"">First param.</param>
+            <returns>Return value.</returns>
+            <exception cref=""T:System.NotSupportedException"">Throws a colourful exception.</exception>
+        </member>
+    </members>
+</doc>";
+        using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(xmlText)))
+        {
+            XmlCommentsReader reader = new XmlCommentsReader(ms);
+            Assert.Single(reader.Document.Members);
+            Assert.Equal("M:ExampleLibrary.Test.GenericMethod()", reader.Document.Members.First().Name);
+            Assert.Equal("T", reader.Document.Members.First().TypeParams.First().Name);
+            Assert.Equal("The type parameter 'T'.", reader.Document.Members.First().TypeParams.First().Description);
+            Assert.Equal("a", reader.Document.Members.First().Params.First().Name);
+            Assert.Equal("First param.", reader.Document.Members.First().Params.First().Description);
+            Assert.Equal("Return value.", reader.Document.Members.First().Returns.Text);
+        }
+    }
+
 
     [Fact]
     public void TestXmlCommentsParsing()
