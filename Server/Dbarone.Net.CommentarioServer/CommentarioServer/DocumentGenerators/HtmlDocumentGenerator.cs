bc568a46fe0f1a68e66ae3ef40818063591ad8fd
davidbarone <davidbarone@live.com>
2024-06-08 16:59:24 +1000

    formatting document

--- Server/Dbarone.Net.CommentarioServer/CommentarioServer/DocumentGenerators/HtmlDocumentGenerator.cs
+++ Server/Dbarone.Net.CommentarioServer/CommentarioServer/DocumentGenerators/HtmlDocumentGenerator.cs
@@ -53,7 +53,7 @@ public class HtmlDocumentGenerator : DocumentGenerator
         <meta charset=""UTF-8"">
         <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
         <meta http-equiv=""X-UA-Compatible"" content=""ie=edge"">
-        <title>HTML 5 Boilerplate</title>
+        <title>Commentario: {GetAssembly().GetName().Name}</title>
 
         <style type=""text/css"">
             {this.Styles}
